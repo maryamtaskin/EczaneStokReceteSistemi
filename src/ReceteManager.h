4d0d62af1207411f55e6143b8305dd58b311f72a
Maryam TASKIN <maryamtaskin1818@gmail.com>
2026-01-05 23:02:55 +0300

    Update ReceteManager.h

--- src/ReceteManager.h
+++ src/ReceteManager.h
@@ -5,16 +5,30 @@
 #include "Recete.h"
 #include "IlacManager.h"
 
+/*
+ * ReceteManager sinifi
+ * Recetelerin olusturulmasi, ilac ekleme islemleri
+ * ve recete listesinin yonetilmesinden sorumludur.
+ */
 class ReceteManager {
 private:
+    // Sistemdeki tum receteler
     vector<Recete> receteler;
+
+    // Otomatik artan son recete numarasi
     int sonReceteNo;
 
 public:
+    // Constructor
     ReceteManager();
 
+    // Yeni recete olusturur ve recete nesnesini dondurur
     Recete* receteOlustur(const Hasta& hasta);
+
+    // Belirtilen receteye ilac ekler
     void receteyeIlacEkle(int receteNo, int barkod, int miktar, IlacManager& ilacManager);
+
+    // Tum receteleri listeler
     void listele();
 };
 

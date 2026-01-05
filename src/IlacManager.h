5c019f661f6ffab9c1046c8b12cab3e51b6a6c72
Maryam TASKIN <maryamtaskin1818@gmail.com>
2026-01-05 23:05:00 +0300

    Update IlacManager.h
    guncelleme

--- src/IlacManager.h
+++ src/IlacManager.h
@@ -6,18 +6,28 @@
 
 using namespace std;
 
+/*
+ * IlacManager sinifi
+ * Ilaclarin eklenmesi, silinmesi ve stok islemlerini yonetir.
+ */
 class IlacManager {
 private:
+    // Sistemdeki tum ilaclar
     vector<Ilac> ilaclar;
 
 public:
+    // Constructor
     IlacManager();
 
+    // Ilac islemleri
     void ilacEkle(const Ilac& ilac);
     void ilacSil(int barkod);
+
+    // Stok islemleri
     void stokArtir(int barkod, int miktar);
     void stokAzalt(int barkod, int miktar);
 
+    // Ilaclari listeler ve barkoda gore ilac bulur
     void listele();
     Ilac* ilacBul(int barkod);
 };
